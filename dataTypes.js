94dd8bd14fe6e7d5e9f955c9ccb9197877d3c994
nepali-rakshya <rakshyanepali440@gmail.com>
2022-04-23 22:20:31 +0530

    datatype

---
+++ dataTypes.js
@@ -0,0 +1,30 @@
+/* ************* DATA TYPES: ***************
+
+1. string
+2. number
+3. undefined
+4. null
+5. boolean
+6. symbol
+7. object
+
+
+************* VARAIBLES: *************
+
+Its a box or container used for storing values.
+
+There are 3 different ways to create variables:
+1. var
+2. let 
+3. const
+
+*/
+
+let myName = "Beau";
+myName = 8;
+
+
+console.log(myName);
+console.log("my name is " + myName);
+
+var a = 9;
