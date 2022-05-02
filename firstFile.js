fcb32b1ba846eae2d96c92cdf6e36c3fd6b72cd7
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-02 21:32:53 +0530

    printFormat

--- firstFile.js
+++ firstFile.js
@@ -1,8 +1,13 @@
 console.log("Start Now! We Will Make It Happen!!");
-var number =5; // in-line comment 
+var number = 5; // in-line comment 
 
 /* this is a 
 multi-line comment */
 
 number = 9;
 console.log(number);
+
+
+let num = 8;
+num = num + 1;
+console.log(num, "times * 1 = 9");
