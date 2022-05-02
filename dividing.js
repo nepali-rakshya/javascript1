404990700ccee33742aeffad165e34e3f9377903
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-03 05:24:25 +0530

    divide

---
+++ dividing.js
@@ -0,0 +1,14 @@
+// it gives quotient as an answer
+// its a divide sign
+
+var rak = 44;
+div = rak / 2;
+console.log(div);
+
+
+// it gives remainder as an answer
+// its a modulus sign
+
+var show = 13;
+divide = show % 2;
+console.log(divide);
