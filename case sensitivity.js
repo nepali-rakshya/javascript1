4446fac0f2ebef5aef15026c65e3f763fe0e8be6
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-03 05:20:23 +0530

    camelCase

---
+++ case sensitivity.js
@@ -0,0 +1,25 @@
+/*javascript is case sensitive in case of variable name and function
+use camelCase to declare variable name
+
+initializing, declaring, uninitialized, assingning value to a var*/
+
+
+
+// Declariations
+var StuDyCASE;
+
+
+// Assignments of var with assignment operator
+STUDYCASE = 10;
+
+console.log(StuDyCASE);
+
+
+// use camelCase for declaring variable or functions
+
+var studyCase;
+studyCase = "Shila";
+console.log(studyCase);
+
+var sum = studyCase + " Rakshya";
+console.log(sum);
