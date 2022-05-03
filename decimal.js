25195580a65f3aabba03983e8baa353afa351ab2
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-03 05:39:16 +0530

    decimal

---
+++ decimal.js
@@ -0,0 +1,9 @@
+/* we don't have different variables for declaring decimal alike in 
+java and python*/
+
+var value = 9.0;
+ans = value / 3.0;
+console.log(ans);
+
+ans = 4.4 / 2;
+console.log(ans);
