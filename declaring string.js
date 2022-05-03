9193e45ca1285147598e039f7f3412a6675a07a0
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-03 05:58:53 +0530

    escaping the quotes

---
+++ declaring string.js
@@ -0,0 +1,13 @@
+var firstName = "Rakshya";
+var lastName = "Nepali";
+
+var myFirstName = 'ram';
+var myLastName = 'shrestha';
+
+console.log(firstName, lastName);
+console.log(myFirstName, myLastName);
+
+/*escaping the quotation in javascript ==== use backslash in front of the quotation*/
+
+var myString = "I am a quote \"but you can add more\" quotes to me\" yeah try it";
+console.log(myString);
