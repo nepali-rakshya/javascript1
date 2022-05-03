521f91554ae6169ee82e08dd0501fe27eaec7f83
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-03 05:45:03 +0530

    decimalIncrement

---
+++ decimal increment.js
@@ -0,0 +1,22 @@
+/* we don't have different variables for declaring decimal alike in 
+java and python*/
+
+var value = 9.0;
+ans = value / 3.0;
+console.log(ans);
+
+ans = 4.4 / 2;
+console.log(ans);
+
+
+/*********************Increment and Decrement******************** */
+var a = 2;
+a++;
+console.log(a);
+
+a--;
+console.log(a);
+
+console.log(++a);
+console.log(a++);
+console.log(a);
--- decimal.js
+++
@@ -1,9 +0,0 @@
-/* we don't have different variables for declaring decimal alike in 
-java and python*/
-
-var value = 9.0;
-ans = value / 3.0;
-console.log(ans);
-
-ans = 4.4 / 2;
-console.log(ans);
