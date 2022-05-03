b76ce4d4a46c0506abfca59d0d7f52f62204cabc
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-03 08:45:16 +0530

    array

---
+++ array.js
@@ -0,0 +1,28 @@
+var ourArray;
+ourArray = ["Rakshya", 67];
+console.log(ourArray);
+
+
+/* Nested array ===== multi dimensional array*/
+
+var ourArray = [
+    ["The universe", "is"],
+    ["listening to", "you", 7]
+];
+console.log(ourArray);
+
+
+// accessing array data with indexes 
+
+var listArray = [50, 60, 70];
+var listData = listArray[0];
+console.log(listData);
+
+console.log(listArray[2]);
+
+
+// modify array with indexes
+
+var arrayList = [1, 2, 3, 4, 5];
+arrayList[2] = 6;
+console.log(arrayList);
