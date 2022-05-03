16eb1cff0f839eebc29649aeb8d9a2eda35d6659
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-03 06:06:22 +0530

    string and quotation

--- declaring string.js
+++ declaring string.js
@@ -11,3 +11,10 @@ console.log(myFirstName, myLastName);
 
 var myString = "I am a quote \"but you can add more\" quotes to me\" yeah try it";
 console.log(myString);
+
+
+/*you can also escape the quotation using single quote in the beginning 
+and double quotation along it*/
+
+var mystrg = 'I am happy "because you have good energy"';
+console.log(mystrg);
