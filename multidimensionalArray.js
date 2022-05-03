072692c6e16bb90768992de59134b38561d2d6d8
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-03 11:54:11 +0530

    multiArray

---
+++ multidimensionalArray.js
@@ -0,0 +1,31 @@
+var multiArray = [
+    [23, 45],
+    [46, 47]
+];
+console.log(multiArray);
+
+// multi dimensional array accessing them with indexes
+
+
+// single array
+var singleArray = [1, 2, 3, 4, 5];
+console.log(singleArray[4]);
+
+
+// multidimensional array
+
+var myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [
+        [10, 11, 12],
+        [777, 77, 7777], 13, 14
+    ],
+    [100, 22, 33]
+];
+
+console.log(myArray[2][2]);
+console.log(myArray[4][2]);
+console.log(myArray[3][1][2]);
+console.log(myArray[3][2]);
