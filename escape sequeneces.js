154c63c98fbbaaf93aca65bad9e122b756f1ebe6
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-03 06:12:21 +0530

    escape sequence

---
+++ escape sequeneces.js
@@ -0,0 +1,20 @@
+/****
+
+CODE OUTPUT
+
+\'  single quote
+\"  doube quote
+\\  backslash
+\n  newline
+\r  carriage return
+\t  tab
+\b  backspace
+\f  form feed
+
+****/
+
+var myStr = "First line\nSecond line\n\t third line with a tab"
+console.log(myStr);
+
+var myStr = "Fourtline with?\r"
+console.log(myStr);
