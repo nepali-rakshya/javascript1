418bce5dfa913ea74101897d4ec314da7dc6c7a5
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-03 07:27:29 +0530

    concatenation

---
+++ concatenation.js
@@ -0,0 +1,14 @@
+var myStr = "I am not the body," + " I am not even the mind.";
+console.log(myStr);
+
+
+// compound operators
+
+var myStr = "I have a definite goal and purpose!";
+myStr += " I will make everything come true with tons of hardwork!"
+
+console.log(myStr);
+
+var a = 11;
+var myStr = "I am an " + a;
+console.log(myStr);
