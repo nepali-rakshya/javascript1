050a1af4054fc323364237d1afc5bc4b57d83a2d
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-03 07:40:22 +0530

    length

---
+++ length of string.js
@@ -0,0 +1,8 @@
+var firstName = "Rakshya Nepali";
+firstName = "Rakesh";
+lastName = "Shrestha";
+// var totalName = 0;
+totalName = firstName + lastName;
+console.log(firstName.length);
+console.log(lastName.length);
+console.log(totalName.length);
