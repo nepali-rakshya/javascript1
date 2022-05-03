fbbbe23d89cd644bd3b9c173d1ee227aba04234a
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-03 05:59:06 +0530

    compoundAssignment

---
+++ compound assignment.js
@@ -0,0 +1,17 @@
+var a = 5;
+a = a + 5;
+console.log(a);
+
+// you can solve this using compound assignment
+
+a += 5;
+console.log(a);
+
+a -= 5;
+console.log(a);
+
+a *= 5;
+console.log(a);
+
+a /= 5;
+console.log(a);
