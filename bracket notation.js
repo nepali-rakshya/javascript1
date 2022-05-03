69ca890f6010bd94d6e8dbd0a897a4180875eb39
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-03 08:03:58 +0530

    finding the letter

---
+++ bracket notation.js
@@ -0,0 +1,20 @@
+var firstLetterOfLastName;
+var lastName = "Lovelace";
+
+firstLetterOfLastName = lastName[5]; //zero based indexing
+console.log(firstLetterOfLastName);
+
+
+/* How to get the last letter of a string*/
+
+var realName = "Rakshya Nepali";
+var finalName = 0;
+finalName = realName[realName.length - 1];
+console.log(finalName);
+
+/* How to get the second last of a string */
+
+var findLength = "Aakash Raj Malhotra";
+var secondLastName = "";
+secondLastName = findLength[findLength.length - 2];
+console.log(secondLastName);
