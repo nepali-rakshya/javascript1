0e2a8236fbe1ce466b035ebdc8f92f55444e23c5
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-10 23:11:56 +0530

    array pop()

---
+++ array pop().js
@@ -0,0 +1,16 @@
+// *manipulating array with pop() which eliminates element from array
+
+
+var ourArray = [1, 2, 3];
+var removedFromOurArray = ourArray.pop();
+console.log(removedFromOurArray);
+console.log(ourArray);
+
+var myArray = [
+    ["John", 23],
+    ["cat", 2]
+];
+var removedFromMyArray;
+removedFromMyArray = myArray.pop();
+console.log(removedFromMyArray);
+console.log(myArray);
