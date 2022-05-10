d73491d376a0b970bf7a8a68cf0c919d97e7e7cd
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-10 22:54:54 +0530

    array push()

--- array push().js
+++ array push().js
@@ -3,3 +3,19 @@
 var arr = [1, 2, 3];
 arr[1] = 5;
 console.log(arr);
+
+
+// *Manipulating array with push()
+
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]);
+console.log(ourArray);
+
+var myArray = [
+    ["John", 23],
+    ["cat", 21]
+];
+myArray.push(["Rakshya", 21]);
+console.log(myArray);
+
+/* you can add one more element into the array with the push() element.*/
