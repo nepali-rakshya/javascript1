3f3e0a0ce497f915547f329fc30c760529cc6e4c
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-10 23:29:59 +0530

    array shift()

---
+++ array shift().js
@@ -0,0 +1,12 @@
+//* shift() array function removes the first set of array 
+
+ourArray = [1, 2, 3];
+removedArray = ourArray.pop();
+removed = ourArray.shift();
+
+console.log(removedArray);
+console.log(removed);
+console.log(ourArray);
+
+addArray = ourArray.push(5);
+console.log(ourArray);
