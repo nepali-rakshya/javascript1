4bcb406746f49d9b1f0d05439249fe8e630b6c05
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-11 23:15:25 +0530

    function

---
+++ FUNCTION/function.js
@@ -0,0 +1,19 @@
+// Example
+
+function ourReusableFunction() {
+    console.log("Heyya, World");
+}
+
+ourReusableFunction();
+ourReusableFunction();
+ourReusableFunction();
+ourReusableFunction();
+
+
+// Second example
+
+function myFunction() {
+    console.log("my name is rakshya nepali");
+}
+
+myFunction();
