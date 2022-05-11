5a49629e985e2235205b935053264a8a257324c7
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-11 23:00:13 +0530

    array unshift()

--- array unshift().js
+++ array unshift().js
@@ -1,6 +1,27 @@
+/*unshift() adds array in front of the array while push() adds to the 
+end of the sentences.*/
+
+
 var typeArr = [
     ["to", "the"],
     ["Queen"]
 ];
 typeArr.unshift(["Bow"]);
 console.log(typeArr);
+
+/*shift() removes array from the front of the array while pop() removes
+array from the end of the sentences.*/
+
+var singleArr = [
+    [1, "Aadi"],
+    ["somebody", "is"],
+    ["dying"]
+];
+
+console.log(singleArr.pop());
+console.log(singleArr);
+
+singleArr.push(["gone mad"]);
+singleArr.shift();
+
+console.log(singleArr);
