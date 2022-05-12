af3d5b8bfea0fce5dfb23b1b0f5357719cae38c5
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-13 00:16:32 +0530

    argument function

--- FUNCTION/argument in function.js
+++ FUNCTION/argument in function.js
@@ -1,7 +1,14 @@
-function reuseableFunction(a, b) {
+function reuseableFunction(a, b) { // parameter
     var sum = 0;
     sum = a + b;
     console.log(sum);
 }
 
-reuseableFunction(10, 5);
+reuseableFunction(10, 5); // passing arguments
+
+
+function functionWithArgs(a, b) {
+    console.log(a + b);
+}
+
+functionWithArgs(100, 5);
