bc82fcdb977eda39ce03733c12b921bbe200a416
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-13 00:24:46 +0530

    global varibale

---
+++ FUNCTION/global variable.js
@@ -0,0 +1,16 @@
+// Declare your variable here
+var myGlobal
+
+
+function fun1(params) {
+    // Assign 5 to oopsGlobal here
+}
+
+// Only change code above this line
+
+function fun2() {
+    var output = "";
+    if (typeof myGlobal != "undefined") {
+        output += "myGlobal: " + myGlobal;
+    }
+}
