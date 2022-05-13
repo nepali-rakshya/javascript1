ced14401c3a2f6aad09ed4bc53a4418b130f12fb
nepali-rakshya <rakshyanepali440@gmail.com>
2022-05-13 07:41:44 +0530

    global variable

--- FUNCTION/global variable.js
+++ FUNCTION/global variable.js
@@ -1,9 +1,13 @@
 // Declare your variable here
-var myGlobal
+var myGlobal = 10;
+/* this is a global variable because it has been declared outside of the function*/
 
 
 function fun1(params) {
     // Assign 5 to oopsGlobal here
+    /* this is a local variable because it has been declared inside of the function*/
+
+    oopsGlobal = 5;
 }
 
 // Only change code above this line
@@ -13,4 +17,11 @@ function fun2() {
     if (typeof myGlobal != "undefined") {
         output += "myGlobal: " + myGlobal;
     }
+    if (typeof oopsGlobal != "undefined") {
+        output += "oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output);
 }
+
+fun1();
+fun2();
